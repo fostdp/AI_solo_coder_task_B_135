ef18a79ee92f64dda8a363905891e660b284f89c
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-17 07:48:26 +0800

    Update project

---
+++ frontend/src/modules/mingtang_3d/index.ts
@@ -0,0 +1,12 @@
+export { default as Mingtang3DModel } from './Mingtang3DModel';
+export {
+  MINGTANG_DIMENSIONS,
+  DEFAULT_WINDOWS,
+  SENSOR_LOCATIONS,
+  WOOD_MATERIAL,
+  WALL_MATERIAL,
+  ROOF_MATERIAL,
+  WINDOW_MATERIAL,
+  GOLD_DECORATION,
+} from './buildingData';
+export type { BuildingDimensions, WindowConfig } from './types';
